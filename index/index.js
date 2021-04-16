fabdee55c04f317a21e204b2adb7e669c284af8b
Baozi960101 <Baozi960101@gmail.com>
2021-04-16 20:49:26 +0800

    first commit

---
+++ index/index.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function () {
+
+
+
+    const title = document.getElementById("title")
+
+    // 兩種用法 一種用querySelector抓class 一種用getElementById抓id
+    const starbutton = document.querySelector(".starButton")
+
+    const calculater = document.getElementById("calculater")
+    calculater.addEventListener("click", function () {
+        title.innerText = "Calculater"
+        starbutton.href = "/calculater/index.html"
+    })
+    const ooxx = document.getElementById("ooxx")
+    ooxx.addEventListener("click", function () {
+        title.innerText = "ooxx"
+        starbutton.href = "/ooxx/index.html"
+    })
+    const stopwatch = document.getElementById("stopwatch")
+    stopwatch.addEventListener("click", function () {
+        title.innerText = "Stopwatch"
+        starbutton.href = "/Stopwatch/index.html"
+        })
+    })
+
+    const meanbar = document.getElementById('meanbar')
+
+    const sidebar = document.getElementById("sidebar")
+    sidebar.addEventListener("click", function () {
+        sidebar.style.right = "-9999px"
+        meanbar.style.right = "-9999px"
+    })
+    
+
+    const mobileheaderContent2 = document.getElementById('mobileheaderContent2')
+    mobileheaderContent2.addEventListener('click',function(){
+        meanbar.style.right = "0px"
+        sidebar.style.right = "0px"
+    })
